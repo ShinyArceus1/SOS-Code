64392ab213642b71f09c4992ae13711cab799fbe
ShinyArceus1 <39779323+ShinyArceus1@users.noreply.github.com>
2018-06-13 14:50:04 +1200

    Update sos.py

--- sos.py
+++ sos.py
@@ -1,19 +1,29 @@
+#
 def shinyspace(paragraphspaces=1):
 	for i in range(paragraphspaces):
 		print("", end="/n")
 
+#print letters/numbers
 def shineprint(message):
 	print(str(message))
 
+#execute
 def shinecute(command):
 	exec(command)
 
+#evaluate
 def shinevaluate(operation):
 	return eval(operation)
 
+#absolute
 def shinelute(operation):
 	return abs(operation)
 
+#sin of a number
 def shinysin(number):
 	import math
 	return math.sin(number)
+
+#divide 2 numbers
+def shinedivide(firstnumber, secondnumber):
+	return firstnumber / secondnumber
