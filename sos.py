c5fe11219d4e97dcb8c2bb83f28b22d9361151bf
ShinyArceus1 <39779323+ShinyArceus1@users.noreply.github.com>
2018-06-13 14:31:11 +1200

    can now execute.

--- sos.py
+++ sos.py
@@ -1,6 +1,9 @@
 def shinyspace(paragraphspaces=1):
-    for i in range(paragraphspaces):
-            print("", end="/n")
+	for i in range(paragraphspaces):
+		print("", end="/n")
 
 def shineprint(message):
-    print(str(message))
+	print(str(message))
+
+def shinecute(command):
+	exec(command)
