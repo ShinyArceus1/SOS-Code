9a70d2a211efaf8e577172eea79c49bd05b1c15b
Richie Bendall <richiebendall@gmail.com>
2018-06-25 17:47:52 +1200

    Added unittest

---
+++ code/test_sos.py
@@ -0,0 +1,10 @@
+import unittest as ut
+import sos
+
+class TestCode(ut.TestCase):
+
+    def test_shinedivide(self):
+        self.assertEqual(s.shinedivide(10, 5), 2)
+
+if __name__ == '__main__':
+    ut.main()
